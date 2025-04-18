e66c906d39a101c99cb1c3143bda41c2818b36a5
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2025-04-18 18:28:16 -0300

    Add appointment form

--- app/appointments/create.tsx
+++ app/appointments/create.tsx
@@ -1,22 +1,46 @@
 import { ActionButton } from "@/components/action-button";
+import AppointmentForm from "@/components/appointment-form";
 import { Separator } from "@/components/ui/separator";
+import { Appointment } from "@/constants/types";
 import { useRouter } from "expo-router";
-import { View, Text } from "react-native";
+import { View, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 
 export default function CreateAppointment() {
   const router = useRouter();
 
   return (
-    <View style={{ padding: 20 }}>
-      <ActionButton
-        text={"Volver A inicio"}
-        callback={() => {
-          router.push("/");
-        }}
-        color={"blue"}
-      />
-      <Separator />
-      <Text>Create appointment</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View style={styles.header}>
+          <ActionButton
+            text={"Volver A inicio"}
+            callback={() => {
+              router.push("/");
+            }}
+            color={"blue"}
+          />
+          <Separator />
+        </View>
+
+        <AppointmentForm
+          onSubmit={(appointment: Omit<Appointment, "id">) => {
+            console.log("To create: ", appointment);
+          }}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContent: {
+    flexGrow: 1,
+    paddingBottom: 30,
+  },
+  header: {
+    padding: 20,
+  },
+});
